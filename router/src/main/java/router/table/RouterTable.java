ec1a71c5893bf51d00d08f859a82e3cc2ec3a286
tihendri <tihendri@student.wethinkcode.co.za>
2020-12-07 18:13:17 +0200

    Sending messages through routing table works

---
+++ router/src/main/java/router/table/RouterTable.java
@@ -0,0 +1,36 @@
+package router.table;
+
+import router.server.Attachment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouterTable {
+    private static final List<Attachment> clients = new ArrayList<>();
+
+    public static void addClient(Attachment client)
+    {
+        clients.add(client);
+    }
+    public static Attachment getClient(int id)
+    {
+        for(Attachment client : clients)
+        {
+            if (client.channelId == id)
+                return client;
+        }
+        return null;
+    }
+    public static int getSize()
+    {
+        return clients.size();
+    }
+    public static void removeClient(int id)
+    {
+        try
+        {
+            clients.remove(getClient(id));
+        }
+        catch(Exception ignored){}
+    }
+}
